e2e6d580893a627bbcd4f22b41d7eeade38acd51
NoNameNoMeme <hasan.valitov@gmail.com>
2024-01-26 21:40:35 +0300

    auth second view

--- pages/login.vue
+++ pages/login.vue
@@ -15,7 +15,7 @@ async function login(username: string, password: string) {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ email: username, password: password }),
     });
 
     if (!response.ok) {
