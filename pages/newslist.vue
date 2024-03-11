59db6fba5aa24b7bcde95861e2e5ffc59f665802
NoNameNoMeme <hasan.valitov@gmail.com>
2024-03-11 14:23:44 +0300

    fix

--- pages/newslist.vue
+++ pages/newslist.vue
@@ -53,7 +53,7 @@ onMounted(() => {
 
         <div class="mb-6 mr-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-2/12">
           <p class="mb-6 text-neutral-500 dark:text-neutral-300">
-            <small>Опубликовано <u>{{ dayjs(newsRef.published_at).locale('ru').format('DD MMMM YYYY') }}</u></small>
+            <small>Опубликовано <u>{{ dayjs(item.published_at).locale('ru').format('DD MMMM YYYY') }}</u></small>
           </p>
         </div>
       </div>
